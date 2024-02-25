b3a5e72ac5f4d52ebdd41218a81a680e83ad5f89
Daria Kornieva <darya.aleksandrovna95@gmail.com>
2024-02-25 16:47:25 +0200

    finished with task 4

---
+++ src/homework/day8/tasks/CarTask.java
@@ -0,0 +1,63 @@
+package homework.day8.tasks;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CarTask {
+
+    List<String> carsList = new ArrayList<>(List.of("Мерс", "Ауди", "Жигуль", "Рено", "Жигуль", "Жигуль", "Ауди"));
+
+    public void writeToFile() {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("cars"))) {
+            for (String i : carsList) {
+                bufferedWriter.write("\"" + i + "\"\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void removeObject() {
+
+        List<String> newCarList = new ArrayList<>();
+
+        for (int i = 0; i < carsList.size(); i++) {
+            if (carsList.get(i).length() <= 4) {
+                newCarList.add(carsList.get(i));
+            }
+        }
+        System.out.println(newCarList);
+    }
+
+    public void removeObjectUsingIterator() {
+
+        Iterator<String> iterator = carsList.iterator();
+
+        while (iterator.hasNext()) {
+            String car = iterator.next();
+            if (car.length() > 4) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public void printListWithSpace () {
+        for (String i : carsList) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        CarTask carTask = new CarTask();
+
+        carTask.writeToFile();
+        carTask.removeObject();
+        carTask.removeObjectUsingIterator();
+        carTask.printListWithSpace();
+    }
+}
